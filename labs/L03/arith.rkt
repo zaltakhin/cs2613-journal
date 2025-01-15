31e725e74c92e17e7b2b05ad38b40c0d7893666f
Zeid Altakhin <a3jf4@remotelabm25.cs.unb.ca>
2025-01-15 08:09:16 -0400

    added test cases for full coverage in arith.rkt

--- labs/L03/arith.rkt
+++ labs/L03/arith.rkt
@@ -22,3 +22,9 @@
 (check-expect (or false false) false)     ; All false, evaluates to false
 (check-expect (or) false)                 ; Empty or, evaluates to false
 
+
+
+#lang htdp/bsl
+(check-expect (my-* 0 5) 0) ; Tests the zero case for a
+(check-expect (my-* 3 0) 0) ; Tests when b is effectively zero
+(check-expect (my-* 2 3) 6) ; Tests a general case
